f80e2fca11ef190cfa40c42d0757091cb611cdb8
Michael Hayder <m.hayder@altimate.net>
2024-07-26 23:04:31 +0200

    Klassen

--- src/main/java/de/company/RedWheel.java
+++ src/main/java/de/company/RedWheel.java
@@ -1,2 +1,7 @@
-package de.company;public class ReadWheel {
+package de.company;
+
+public class RedWheel {
+    int red = 100;
+    int blue = 0;
+    int green = 0;
 }
--- src/main/java/de/company/SilverWheel.java
+++ src/main/java/de/company/SilverWheel.java
@@ -1,4 +1,8 @@
 package de.company;
 
 public class SilverWheel {
+    int red = 33;
+    int blue = 33;
+    int green = 33;
+
 }
